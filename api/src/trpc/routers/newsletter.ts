61e12c82449a8e64a262a801dfeb33ca8c1fb7a2
irere123 <irere2020@outlook.com>
2025-07-09 14:24:50 +0200

    feat(api): add subscribe, unsubscribe and confirmation

--- api/src/trpc/routers/newsletter.ts
+++ api/src/trpc/routers/newsletter.ts
@@ -1,7 +1,113 @@
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import { subscribers } from "@/schemas";
 import { createTRPCRouter, publicProcedure } from "@/trpc/init";
+import { generateId } from "@/lib/utils";
 
 export const newsletterRouter = createTRPCRouter({
-  subscribe: publicProcedure.query(() => {
-    return "Subscribed";
-  }),
+  subscribe: publicProcedure
+    .input(
+      z.object({
+        email: z.string().email(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const [sub] = await ctx.db
+        .select()
+        .from(subscribers)
+        .where(eq(subscribers.email, input.email))
+        .limit(1);
+
+      if (!sub) {
+        throw new TRPCError({
+          message: "Already joined the newsletter.",
+          code: "BAD_REQUEST",
+        });
+      }
+
+      try {
+        await ctx.db.insert(subscribers).values({
+          id: generateId(),
+          email: input.email,
+          token: generateId("st"),
+        });
+      } catch (error) {
+        console.log(error);
+        throw new TRPCError({
+          message: "Something went wrong",
+          code: "INTERNAL_SERVER_ERROR",
+        });
+      }
+
+      return {
+        success: true,
+      };
+    }),
+  unsubscribe: publicProcedure
+    .input(
+      z.object({
+        token: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const sub = await ctx.db
+        .select()
+        .from(subscribers)
+        .where(eq(subscribers.token, input.token));
+
+      if (!sub) {
+        throw new TRPCError({
+          message: "Subscription not found",
+          code: "NOT_FOUND",
+        });
+      }
+
+      try {
+        await ctx.db
+          .update(subscribers)
+          .set({ isUnsubscribed: true, unsubscribedAt: new Date() });
+      } catch (error) {
+        console.log(error);
+        throw new TRPCError({
+          message: "Something went wrong",
+          code: "INTERNAL_SERVER_ERROR",
+        });
+      }
+
+      return {
+        success: true,
+      };
+    }),
+  confirmation: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const sub = await ctx.db
+        .select()
+        .from(subscribers)
+        .where(eq(subscribers.token, input.token));
+
+      if (!sub) {
+        throw new TRPCError({
+          message: "Subscription not found",
+          code: "NOT_FOUND",
+        });
+      }
+
+      try {
+        await ctx.db
+          .update(subscribers)
+          .set({ confirmedAt: new Date(), isConfirmed: true });
+      } catch (error) {
+        console.log(error);
+        throw new TRPCError({
+          message: "Something went wrong",
+          code: "INTERNAL_SERVER_ERROR",
+        });
+      }
+
+      return {
+        success: true,
+      };
+    }),
 });
