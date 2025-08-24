f42315adc9c8ecde41bc25094f078aa8c4eb1c67
irere123 <irere2020@outlook.com>
2025-08-24 20:32:24 +0200

    feat(web): add editorial layout

--- web/src/routes/(editorial)/documents/page.tsx
+++ web/src/routes/(editorial)/documents/page.tsx
@@ -1,7 +1,17 @@
+import type { Route } from "../+types/layout";
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: "Documents" }];
+}
+
 export default function DocumentsPage() {
   return (
-    <div>
-      <p>Documents</p>
+    <div className="w-full max-w-3xl mx-auto">
+      <div>
+        <h3>Documents</h3>
+        <button>Create new document</button>
+      </div>
+      <div></div>
     </div>
   );
 }
