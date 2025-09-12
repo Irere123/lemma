c015076a6be1276dd7d1d68079bf0d9761d28f61
irere123 <irere2020@outlook.com>
2025-09-12 19:51:46 +0200

    feat(web): add navigation nprogess bar

--- web/src/providers/index.tsx
+++ web/src/providers/index.tsx
@@ -1,5 +1,7 @@
+import { useNProgress } from "@/hooks/use-nprogress";
 import { TRPCReactProvider } from "@/trpc/client";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  useNProgress();
   return <TRPCReactProvider>{children}</TRPCReactProvider>;
 }
