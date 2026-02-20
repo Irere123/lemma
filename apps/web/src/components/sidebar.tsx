6698368e695b72d0e9ee6a44c77d1195a6d0ef66
irere123 <irere2020@outlook.com>
2026-02-20 07:45:06 +0200

    refactor: new sidebar styles

--- apps/web/src/components/sidebar.tsx
+++ apps/web/src/components/sidebar.tsx
@@ -13,23 +13,18 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 export function Sidebar() {
   return (
-    <div className='flex flex-col gap-4 h-screen sticky border-r border-neutral-200 sm:w-72 w-16 top-0 z-50'>
+    <div className='flex flex-col gap-4 h-screen sticky border-r px-2 border-neutral-200 top-0 z-50'>
       <div className='flex items-center justify-between p-4 border-b border-neutral-200'>
-        <h2 className='text-xl font-bold'>Lemma</h2>
         <AccountDropdown />
       </div>
-      <div className='flex gap-2 px-2'>
+      <div className='flex flex-col gap-2 flex-1'>
         <SidebarLink href='/app' icon={<IconAlignJustified size={20} />} />
         <SidebarLink href='/app/search' icon={<IconSearch size={20} />} />
         <SidebarLink href='/app/new' icon={<IconPlus size={20} />} />
         <SidebarLink href='/u/profile' icon={<IconUserCircle size={20} />} />
         <SidebarLink href='/app/settings' icon={<IconSettings size={20} />} />
       </div>
-      <div className='flex flex-1 px-2.5 flex-col gap-2'>
-        <h2 className='text-md font-semibold'>Archive</h2>
-        {/* <DocumentsHistoryList /> */}
-      </div>
-      <div className='flex items-center gap-2 p-2'>
+      <div className='flex items-center justify-center gap-2 p-2'>
         <Avatar className='rounded-full size-9'>
           <AvatarImage
             alt='User'
@@ -37,10 +32,6 @@ export function Sidebar() {
           />
           <AvatarFallback>AV</AvatarFallback>
         </Avatar>
-        <div className='flex flex-col text-sm'>
-          <p className='font-semibold'>Irere Emmanuel</p>
-          <p className='text-muted-foreground'>irere@lemma.ai</p>
-        </div>
       </div>
     </div>
   )
@@ -58,7 +49,7 @@ const SidebarLink = ({
   return (
     <Link
       to={href}
-      className={cn('p-2 rounded-md', className)}
+      className={cn('p-2 rounded-md flex items-center justify-center', className)}
       activeProps={{ className: 'bg-muted' }}
     >
       {icon}
